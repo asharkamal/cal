39f1609ef0ae7a2c7124d13e4f3d66a66d0cc5f0
ashar <ashar@192.168.1.7>
2018-02-13 14:14:26 +0200

    calc

---
+++ src/calc/Calculatrohm.java
@@ -0,0 +1,9 @@
+package calc;
+
+public class Calculatrohm {
+public static void main( String argv []) {
+	CalculatorhmModal m=new CalculatorhmModal();
+	CalculatorhmViwe v=new CalculatorhmViwe();
+	CalculatorhmController c=new CalculatorhmController(m,v);
+}
+}
